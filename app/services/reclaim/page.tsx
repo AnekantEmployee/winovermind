8abd04eaa2058c6a6aaf4e4f1f3ee5e10bcc2fc6
Anekant Jain <anekant.employee@gmail.com>
2026-02-19 11:35:35 +0530

    `Added Image component to ReclaimService page`

--- app/services/reclaim/page.tsx
+++ app/services/reclaim/page.tsx
@@ -2,12 +2,22 @@ import HeroSection from "@/components/Services/Reclaim/HeroSection";
 import ExperienceSection from "@/components/Services/Reclaim/ExperienceSection";
 import WhoIsItForSection from "@/components/Services/Reclaim/WhoIsItForSection";
 import WhyReclaimSection from "@/components/Services/Reclaim/WhyReclaimSection";
+import Image from "next/image";
 
 export default function ReclaimService() {
   return (
     <>
       <HeroSection />
       <WhyReclaimSection />
+      <div className="w-full">
+        <Image
+          src="/images/services/3d84ee65-13af-4caf-a2a0-b07994c306b2 3.png"
+          alt="Reclaim journey"
+          width={10000}
+          height={10000}
+          className="w-full h-auto"
+        />
+      </div>
       <WhoIsItForSection />
       <ExperienceSection />
     </>
